41f8314c999462507a0d39cdbe8fb43df2063438
K.Misaki <pro.mickey.12091it@gmail.com>
2015-11-08 19:35:11 +0900

    shell scripts directory path changed

--- .bin/android_test.sh
+++ .bin/android_test.sh
@@ -2,5 +2,5 @@
 # repository root
 dir=$(cd $(dirname $0); pwd)
 
-cd $dir/projects/AndroidLibApp
+cd $dir/../projects/AndroidLibApp
 ./gradlew assembleDebug
--- .bin/commons_test.sh
+++ .bin/commons_test.sh
@@ -2,5 +2,5 @@
 # repository root
 dir=$(cd $(dirname $0); pwd)
 
-cd $dir/projects/common-library
+cd $dir/../projects/common-library
 ./gradlew clean build uploadArchives
